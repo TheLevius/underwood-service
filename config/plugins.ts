a4df87edb0ee6bf830f87d69cd1efe263a501650
TheLevius <reflevius@gmail.com>
2025-04-12 18:50:30 +0300

    init: strapi init settings

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default () => ({
+	seo: {
+		enabled: true,
+	},
+	'webp-converter': {
+		enabled: true,
+		config: {
+			mimeTypes: ['image/png', 'image/jpeg', 'image/jpg'],
+		},
+	},
+});
